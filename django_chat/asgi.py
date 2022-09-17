c55e67118c19713af28be43c43589ca43a8698a8
Manav1011 <manavshah1011.ms@gmail.com.com>
2022-09-17 16:51:42 +0530

    None

--- django_chat/asgi.py
+++ django_chat/asgi.py
@@ -10,14 +10,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 import os
 import django
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_chat.settings')
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.sessions import SessionMiddlewareStack
 import accounts.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_chat.settings')
-django.setup()
 
 application=get_asgi_application()
 application=ProtocolTypeRouter({
